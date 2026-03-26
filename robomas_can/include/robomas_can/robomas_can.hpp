e82a4c4de516aa746738e3533e72cf5627a2bbe4
kni <ayuayugonbe@gmail.com>
2026-03-26 21:01:54 +0900

    feat:基底クラスのヘッダーファイル追加。

--- robomas_can/include/robomas_can/robomas_can.hpp
+++ robomas_can/include/robomas_can/robomas_can.hpp
@@ -1,14 +1,55 @@
+#pragma once
 #include "gn10_can/core/can_bus.hpp"
-#include "gn10_can/core/can_device.hpp"
 
-class RobomasCAN : public gn10_can::CANDevice
+namespace robomas_can {
+
+// robomas共通の送信CANid
+#define SEND_CANID 0x200
+
+// M2006　M3508の制御フレーム（送信）
+#define SEND_CANID_1_4 0x200
+#define SEND_CANID_5_8 0x1FF
+
+// motorに送る電流値を格納する配列
+struct MotorCurrent {
+    int16_t current[4];
+} __attribute__((__packed__));
+
+// c610とc620に対応するための基底クラス
+class RobomasCAN
 {
+private:
+    gn10_can::CANBus& bus_;
+    gn10_can::CANFrame frame_;
+    MotorCurrent current_[2];
+
 public:
     /**
-     * @brief ロボマスモーター用コンストラクタ
-     *
-     * @param bus CANBusクラスの参照
-     * @param dev_id デバイスID
+     * @brief CAN通信用クラスのコンストラクタ
      */
-    RobomasCAN(gn10_can::CANBus& bus, uint8_t dev_id);
+    RobomasCAN(gn10_can::CANBus& bus_);
+
+    virtual ~RobomasCAN() = default;
+
+    /**
+     * @brief MotorCurrent用のget関数
+     *
+     * @param motor_number 何番目のロボマスモーターか判断する。
+     * 値域は1-8。
+     */
+    virtual int16_t get_current(uint8_t motor_number) = 0;
+
+    /**
+     * @brief escからのfeedbackを受け取る関数
+     * @param can_id feedback先のESCのid
+     * @param data 受け取ったデータ
+     */
+    void receive_data(uint16_t can_id, uint8_t data[8]);
+
+    /**
+     * @brief escにデータを送る関数
+     * @details get_currentで取得した電流値を送信します。
+     */
+    void send_data();
 };
+}  // namespace robomas_can
