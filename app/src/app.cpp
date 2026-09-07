a3c5e60712f35c80e8dc3ec5d6c1abb4c869bc0d
aiba-gento <8gn24gn25@gmail.com>
2026-09-07 21:51:52 +0900

    fix:feedbackの要素名変更に伴う修正

--- app/src/app.cpp
+++ app/src/app.cpp
@@ -333,10 +333,8 @@ void loop()
 
     gn10_can::devices::power_manager::Sensor drive_power_sensor{};
     if (drive_power_manager.get_new_sensor(drive_power_sensor)) {
-        feedback_.drive_battery_voltage_mv = static_cast<uint16_t>(
-            std::clamp(drive_power_sensor.voltage * 1000.0f, 0.0f, (float)UINT16_MAX)
-        );
-        feedback_.drive_battery_current = drive_power_sensor.current;
+        feedback_.drive_battery_voltages = drive_power_sensor.voltage;
+        feedback_.drive_current          = drive_power_sensor.current;
     }
     gn10_can::devices::power_manager::Status drive_power_status{};
     if (drive_power_manager.get_new_status(drive_power_status)) {
@@ -345,10 +343,9 @@ void loop()
     }
     std::array<float, 4> voltages;
     if (logic_power_manager.get_new_voltages(voltages)) {
-        feedback_.logic_power_voltage_mv       = voltages[0];
-        feedback_.logic_battery_voltages_mv[1] = voltages[1];
-        feedback_.logic_battery_voltages_mv[2] = voltages[2];
-        feedback_.logic_battery_voltages_mv[3] = voltages[3];
+        feedback_.logic_battery_voltages[1] = voltages[1];
+        feedback_.logic_battery_voltages[2] = voltages[2];
+        feedback_.logic_battery_voltages[3] = voltages[3];
     }
 
     read_button_and_send_debug_pc_packet();
