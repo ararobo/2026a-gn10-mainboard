7a0764cc74f17b62e645a407f6ea93c1f67fc2c6
aiba-gento <8gn24gn25@gmail.com>
2026-09-07 21:44:23 +0900

    feat:シーケンス番号を追加し、変数名をわかりやすく修正

--- app/include/app/robot_config.hpp
+++ app/include/app/robot_config.hpp
@@ -84,18 +84,19 @@ static_assert(sizeof(command_t) == 32);
  *
  */
 struct feedback_t {
-    uint8_t header;  // ヘッダー
-    uint8_t reserved;
-    uint16_t drive_battery_voltage_mv;
-    uint16_t logic_power_voltage_mv;
-    uint16_t logic_battery_voltages_mv[3];
-    float drive_battery_current;
-    float belt_vel_last;
-    float wheel_angular_velocity[3];  // 0:front 1:left 2:right
-    float loading_belt_angle;
-    float bucket_arm_hight;
+    uint8_t header;    // ヘッダー
+    uint8_t sequence;  // シーケンス番号
+    // 電源周り
     bool emergency_stop_enabled;
     bool over_current;
+    float drive_battery_voltages;
+    float logic_battery_voltages[2];
+    float drive_current;
+    // 各アクチュエータ
+    float wheel_angular_velocity[3];  // 0:front 1:left 2:right
+    float belt_launcher_velocity;     // [m/s]
+    float loading_belt_angle;         // 装填機構のプーリー角度[rad]
+    float bucket_arm_hight;           // バケツアームの高さ[m]
 } __attribute__((__packed__));
 
 union feedback_u {
@@ -103,7 +104,7 @@ union feedback_u {
     uint8_t binary[sizeof(feedback_t)];
 } __attribute__((__packed__));
 
-static_assert(sizeof(feedback_t) == 42);
+static_assert(sizeof(feedback_t) == 44);
 
 /**
  * @brief 操縦デバイスのレバーの傾きと押し込み
