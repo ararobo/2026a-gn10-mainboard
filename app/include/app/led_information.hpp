5112dd7af7e2b2d436e4edca17146a8d5676764d
aiba-gento <8gn24gn25@gmail.com>
2026-09-10 09:03:23 +0900

    feat:LEDとの通信データを修正

--- app/include/app/led_information.hpp
+++ app/include/app/led_information.hpp
@@ -1,12 +1,12 @@
 #pragma once
-#include <cstdint>
+#include <array>
 
 struct LEDInformation {
     /*ベルト直動*/
-    bool belt_initialization{};
+    bool belt_initialization{};  // true:打てます！！ false:初期化未完了
     float belt_velocity{};
     /*エアー射出*/
-    bool air_injection{};  // true:injection false:not injection
+    bool air_injection{};
     /*電圧*/
-    std::array<float, 3> battery_voltage{};  // 0,1:logic 2:drive
+    float battery_voltage[3]{};  // 0,1:logic 2:drive
 } __attribute__((__packed__));
